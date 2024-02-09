3f8a00e435f0b7e1eb6fc7290cba47bdbdc8396c
Sam Dippie <samuel.dippie@missionintelligence.co.nz>
2024-02-09 13:01:42 +1300

    updated code from chatgpt

--- BMI.py
+++ BMI.py
@@ -1,5 +1,5 @@
 # Python code to crete BMI Calc funcationality 
-
+""""
 import math as m 
 
 a = 2
@@ -24,4 +24,29 @@ def BMI ():
 
 
 BMI()
+"""
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def calculate_bmi(weight_kg, height_cm):
+    height_m = height_cm / 100
+    bmi = weight_kg / (height_m ** 2)
+    return bmi
+
+@app.route('/')
+def index():
+    return render_template('BMI.html')
+
+@app.route('/calculate_bmi', methods=['POST'])
+def calculate():
+    weight = float(request.form['weight'])
+    height = float(request.form['height'])
+    bmi = calculate_bmi(weight, height)
+    return f'Your BMI is: {bmi:.2f}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
